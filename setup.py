27e2988db3d5c15ae3a79c1e241d0a903fff8b44
taketo <104896019+artorixx@users.noreply.github.com>
2024-05-20 04:09:50 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+def requirements_from_file(file_name):
+    return open(file_name).read().splitlines()
+
+setup(
+    name='tsr',
+    packages=find_packages(),
+    install_requires=requirements_from_file('requirements.txt'),
+)
